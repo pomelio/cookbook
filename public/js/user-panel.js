a7c8d76e3b84d389a08f88919706cec6d14b05d7
jaynsw <125770399+chatsarah@users.noreply.github.com>
2023-09-03 23:02:47 +1000

    df

--- public/js/user-panel.js
+++ public/js/user-panel.js
@@ -45,11 +45,11 @@ $(document).ready(
                 <li>
                     <a href="/login"
                         class="block px-4 py-2 text-sm text-gray-700 inline-flex hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-                        role="menuitem"><div class="flex items-center ml-1">
+                        role="menuitem">
                         <svg class="w-[12px] h-[12px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 15">
     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M1 7.5h11m0 0L8 3.786M12 7.5l-4 3.714M12 1h3c.53 0 1.04.196 1.414.544.375.348.586.82.586 1.313v9.286c0 .492-.21.965-.586 1.313A2.081 2.081 0 0 1 15 14h-3"/>
   </svg>Log In
-                    </div></a>
+                    </a>
                 </li>
                 
                 
