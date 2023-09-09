9dce870e2a8e635ffe03831dcea4471cef2f20f4
jaynsw <125770399+chatsarah@users.noreply.github.com>
2023-09-09 11:43:28 +1000

    df

--- public/js/comment-input.js
+++ public/js/comment-input.js
@@ -5,7 +5,7 @@ $(document).on('my-account', (event, account) => {
 
     function renderInput() {
         let html = `
-<div class="flex flex-col items-end p-4 my-6 mx-4 bg-white rounded-lg shadow md:justify-between md:p-6 xl:p-8 dark:bg-gray-800">
+<div id="comment-input" class="flex flex-col items-end p-4 my-6 mx-4 bg-white rounded-lg shadow md:justify-between md:p-6 xl:p-8 dark:bg-gray-800">
 
     <label for="message" class="justify-self-start block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Message</label>
     <textarea id="comment-message" rows="4" class="block mb-2 p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment..."></textarea>
@@ -19,7 +19,7 @@ $(document).on('my-account', (event, account) => {
             var currentURL = encodeURIComponent(window.location.href);
             var loginURL = `/login?redirect=${currentURL}`;
             html = `
-<div class="flex flex-col items-end p-4 my-6 mx-4 bg-white rounded-lg shadow md:justify-between md:p-6 xl:p-8 dark:bg-gray-800">
+<div id="comment-input" class="flex flex-col items-end p-4 my-6 mx-4 bg-white rounded-lg shadow md:justify-between md:p-6 xl:p-8 dark:bg-gray-800">
 
     <label for="message" class="justify-self-start block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Message</label>
     <textarea disabled id="comment-message" rows="4" class="block mb-2 p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment..."></textarea>
